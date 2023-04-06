eb2e7d8f0c521e6454aa94a0326ea84772ca6c92
rupom <furqanrupom978@gmail.com>
2023-04-07 02:03:27 +0600

    added cart components and we can add shirts in add to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const Cart = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+const Cart = ({ cart, removeCart}) => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center">{cart.length}</h1>
+      <div>
+        {cart.map((shirt) => (
+          <div
+            key={shirt._id}
+            className="shadow p-3 rounded flex justify-between items-center"
+          >
+            <p className="text-xl font-bold">{shirt.name}</p>
+            <button
+              onClick={() => removeCart(shirt._id)}
+              className="font-semibold px-7 py-3 rounded-md bg-slate-900 text-white my-4"
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Shirt from '../Shirt/Shirt';
+import Cart from '../Cart/Cart'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
     const shirts = useLoaderData()
     const [cart,setCart] = useState([])
     const handleCart = (shirts) =>{
         const newCart = [...cart,shirts]
-        setCart(newCart)
+        const isAdded = cart.some(exists => exists._id === shirts._id)
+        if(!isAdded){
+            setCart(newCart)
+        }else {toast('you can add one shirts')}
     }
-    console.log(cart)
+    const removeCart = (id) =>{
+        const selectedShirt = cart.filter(cd => cd._id !== id)
+        setCart(selectedShirt)
+    }
+
     return (
         <section className="grid grid-cols-[3fr,1fr] gap-10 my-20">
             <div className="grid md:grid-cols-3 gap-10">
@@ -18,8 +28,9 @@ const Home = () => {
                 }
             </div>
             <div>
-
+               <Cart cart={cart}  removeCart={removeCart}></Cart>
             </div>
+            <ToastContainer></ToastContainer>
         </section>
     );
 };
