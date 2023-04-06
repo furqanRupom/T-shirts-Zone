413d9d6798ff67d65a5720f0bca80b8a426df332
rupom <furqanrupom978@gmail.com>
2023-04-06 23:41:06 +0600

    added cart componets for calculation of shirts and add to cart

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import Shirt from '../shirt/shirt';
+import Shirt from '../Shirt/Shirt';
 
 const Home = () => {
     const shirts = useLoaderData()
+    const [cart,setCart] = useState([])
+    const handleCart = (shirts) =>{
+        const newCart = [...cart,shirts]
+        setCart(newCart)
+    }
+    console.log(cart)
     return (
         <section className="grid grid-cols-[3fr,1fr] gap-10 my-20">
             <div className="grid md:grid-cols-3 gap-10">
                 {
-                    shirts.map(shirt => <Shirt key={shirt._id} shirt={shirt} />)
+                    shirts.map(shirt => <Shirt key={shirt._id} shirt={shirt} handleCart={handleCart} />)
                 }
             </div>
             <div>
--- src/components/Shirt/Shirt.jsx
+++ src/components/Shirt/Shirt.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Shirt = ({shirt}) => {
+const Shirt = ({shirt,handleCart}) => {
     const {price,picture,name} = shirt
     return (
-        <div className="p-4 border-4 rounded-lg mx-auto">
+        <div className="p-4 border-1 shadow rounded-lg mx-auto">
             <img className="h-[250px] mx-auto" src={picture} alt="logo" />
             <h1 className="text-xl font-bold py-3 ">
                 name : {name}
             </h1>
+            <h4 className="text-lg font-bold">
+                price : {price}
+            </h4>
+            <button onClick={() => handleCart(shirt)} className="font-semibold px-7 py-3 rounded-md bg-slate-900 text-white my-4">
+                Buy Now
+            </button>
         </div>
     );
 };
